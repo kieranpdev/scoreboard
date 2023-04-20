f0eb5bc7b3149ac9475e982aee3901e811b163a0
Kieran Price <122171217+kieranpdev@users.noreply.github.com>
2023-04-20 19:04:33 +0100

    Added props to Header, Player and Counter

--- app.js
+++ app.js
@@ -1,30 +1,32 @@
-const Header = () => {
+const Header = (props) => {
     return (
         <header>
-            <h1>Scoreboard</h1>
-            <span className="stats">Players: 1</span>
+            <h1>{ props.title }</h1>
+            <span className="stats">Players: { props.totalPlayers }</span>
         </header>
     );
 }
 
-const Counter = () => {
+const Counter = (props) => {
     return (
         <div className="counter">
             <button className="counter-action decrement"> - </button>
-            <span className="counter-score">35</span>
+            <span className="counter-score">{props.score}</span>
             <button className="counter-action increment"> + </button>
         </div>
     );
 }
 
-const Player = () => {
+const Player = (props) => {
     return (
         <div className="player">
             <span className="player-name">
-                Kieran
+                {props.name}
             </span>
 
-            <Counter />
+            <Counter 
+                score={props.score}
+            />
         </div>
     );
 }
@@ -32,10 +34,16 @@ const Player = () => {
 const App = () => {
     return (
         <div className="scoreboard">
-            <Header />
+            <Header 
+            title="Scoreboard" 
+            totalPlayers={2}
+            />
 
             {/* Players list */}
-            <Player />
+            <Player name="Kieran" score="55" />
+            <Player name="Alex" score="70" />
+            <Player name="Jessica" score="65" />
+            <Player name="Carrie" score="60" />
         </div>
     );
 }
